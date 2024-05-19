f8b3af28195b8f534ee6f43630da199121ee43f9
alimurad4555 <alimurad4555gmail.com>
2024-05-19 00:21:22 -0700

    funchion

---
+++ main.js
@@ -0,0 +1,33 @@
+// Function without parameter or argumeters
+//function myfan(){
+//    console.log('switch on');
+//    console.log('hawa strat');
+//
+//}
+//myfan()
+//
+//function mysum(){
+//    let a =10;
+//    let b =20;
+//    console.log(a+b);
+//    
+//}
+//mysum()
+// function with argumeters and parameters
+//function print(garting:string){
+//    console.log(garting + 'ali');    
+//}
+//print('hello')
+//function mysum(a:number,b:number){
+//    let sum = a + b;
+//    console.log(sum);
+//}
+//mysum(10 + 20);
+// retrun
+function mysum() {
+    var a = 5;
+    var b = 10;
+    var sum = a + b;
+    return sum;
+}
+console.log(mysum());
---
+++ main.ts
@@ -0,0 +1,43 @@
+// Function without parameter or argumeters
+
+//function myfan(){
+//    console.log('switch on');
+//    console.log('hawa strat');
+//
+//}
+//myfan()
+//
+//function mysum(){
+//    let a =10;
+//    let b =20;
+//    console.log(a+b);
+//    
+//}
+//mysum()
+
+// function with argumeters and parameters
+
+//function print(garting:string){
+//    console.log(garting + 'ali');    
+//}
+//print('hello')
+
+//function mysum(a:number,b:number){
+//    let sum = a + b;
+//    console.log(sum);
+//}
+//mysum(10 + 20);
+
+// retrun
+//function mysum(){
+//    let a =5;
+//    let b =10;
+//    let sum = a + b;
+//
+//    return sum;
+//}
+//console.log(mysum());
+
+
+
+
